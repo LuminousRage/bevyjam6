6a1ea5fa82b72a0e2c8993b950ee228dd4da6d3a
LuminousRage <56435875+LuminousRage@users.noreply.github.com>
2025-06-04 17:30:09 +1000

    ♻️ chore: tweak config a lil

--- src/player/configs.rs
+++ src/player/configs.rs
@@ -1,12 +1,12 @@
 use avian2d::math::Scalar;
 
-pub const MOVEMENT_ACCELERATION: f32 = 5000.0;
+pub const MOVEMENT_ACCELERATION: f32 = 2500.0;
 pub const DASH_SPEED_MODIFIER: f32 = 12.5;
-pub const JUMP_IMPULSE: f32 = 1100.0;
+pub const JUMP_IMPULSE: f32 = 1000.0;
 pub const MOVEMENT_DAMPING: f32 = 6.0;
 pub const MAX_SLOPE_ANGLE: f32 = (30.0 as Scalar).to_radians();
 pub const CHARACTER_GRAVITY_SCALE: f32 = 1.5;
 pub const DASH_DURATION_MILLISECONDS: u64 = 200;
-pub const JUMP_DURATION_MILLISECONDS: u64 = 500;
+pub const JUMP_DURATION_MILLISECONDS: u64 = 600;
 
 pub const CHARACTER_HEALTH: f32 = 100.0;
