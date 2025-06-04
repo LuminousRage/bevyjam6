b2466b99e7e04d2f26f87eee8559e02f4d690788
LuminousRage <56435875+LuminousRage@users.noreply.github.com>
2025-06-04 22:34:50 +1000

    ✨ feat: Add camera smooth nudge

--- src/camera.rs
+++ src/camera.rs
@@ -7,20 +7,26 @@ pub(super) fn plugin(app: &mut App) {
     app.add_systems(Update, update_camera);
 }
 
+/// How quickly should the camera snap to the desired location.
+const CAMERA_DECAY_RATE: f32 = 2.;
+
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((Name::new("Camera"), Camera2d));
 }
 
 fn update_camera(
-    mut camera_query: Query<&mut Transform, With<Camera2d>>,
-    player_query: Query<&Transform, (With<Player>, Without<Camera2d>)>,
+    camera_query: Single<&mut Transform, With<Camera2d>>,
+    player_query: Option<Single<&Transform, (With<Player>, Without<Camera2d>)>>,
+    time: Res<Time>,
 ) {
-    let mut camera_transform = camera_query
-        .single_mut()
-        .expect("There should be only one camera. If this has changed, redo this system.");
+    let mut camera_transform = camera_query;
 
-    if let Ok(player_transform) = player_query.single() {
-        // Center the camera on the player
-        camera_transform.translation = player_transform.translation;
+    if let Some(player_transform) = player_query {
+        let Vec3 { x, y, .. } = player_transform.translation;
+        let direction = Vec3::new(x, y, camera_transform.translation.z);
+
+        camera_transform
+            .translation
+            .smooth_nudge(&direction, CAMERA_DECAY_RATE, time.delta_secs());
     }
 }
--- src/player/configs.rs
+++ src/player/configs.rs
@@ -1,7 +1,7 @@
 use avian2d::math::Scalar;
 
 pub const MOVEMENT_SPEED: f32 = 500.0;
-pub const DASH_SPEED_MODIFIER: f32 = 85.5;
+pub const DASH_SPEED_MODIFIER: f32 = 60.;
 pub const JUMP_IMPULSE: f32 = 800.0;
 pub const MOVEMENT_DAMPING: f32 = 6.0;
 pub const MAX_SLOPE_ANGLE: f32 = (30.0 as Scalar).to_radians();
