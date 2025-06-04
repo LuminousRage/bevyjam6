64792df9d309de849ecfb79dfe78a1d171335f86
4321louis <45321dumkof@gmail.com>
2025-06-04 16:37:31 +1000

    Update configs.rs

--- src/player/configs.rs
+++ src/player/configs.rs
@@ -1,7 +1,7 @@
 use avian2d::math::Scalar;
 
 pub const MOVEMENT_ACCELERATION: f32 = 5000.0;
-pub const DASH_SPEED_MODIFIER: f32 = 20;
+pub const DASH_SPEED_MODIFIER: f32 = 20.0;
 pub const JUMP_IMPULSE: f32 = 1100.0;
 pub const MOVEMENT_DAMPING: f32 = 6.0;
 pub const MAX_SLOPE_ANGLE: f32 = (30.0 as Scalar).to_radians();
