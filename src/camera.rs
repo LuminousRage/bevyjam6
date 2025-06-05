370beab42c38f1559d0ca98ecf27fcdd12875009
LuminousRage <56435875+LuminousRage@users.noreply.github.com>
2025-06-05 13:36:41 +1000

    ✨ feat: tweak min camera

--- src/camera.rs
+++ src/camera.rs
@@ -9,6 +9,7 @@ pub(super) fn plugin(app: &mut App) {
 
 /// How quickly should the camera snap to the desired location.
 const CAMERA_DECAY_RATE: f32 = 2.;
+const FLOOR_MIN_Y: f32 = 100.0;
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((Name::new("Camera"), Camera2d));
@@ -22,8 +23,9 @@ fn update_camera(
     let mut camera_transform = camera_query;
 
     if let Some(player_transform) = player_query {
+        // TODO: Add track player when player falls off
         let Vec3 { x, y, .. } = player_transform.translation;
-        let direction = Vec3::new(x, y, camera_transform.translation.z);
+        let direction = Vec3::new(x, y.max(FLOOR_MIN_Y), camera_transform.translation.z);
 
         camera_transform
             .translation
--- src/player/configs.rs
+++ src/player/configs.rs
@@ -2,8 +2,8 @@ use avian2d::math::Scalar;
 use bevy::input::keyboard::KeyCode;
 
 pub const MOVEMENT_SPEED: f32 = 500.0;
-pub const DASH_SPEED_MODIFIER: f32 = 60.;
-pub const JUMP_IMPULSE: f32 = 800.0;
+pub const DASH_SPEED_MODIFIER: f32 = 80.;
+pub const JUMP_IMPULSE: f32 = 1000.0;
 pub const MOVEMENT_DAMPING: f32 = 6.0;
 pub const MAX_SLOPE_ANGLE: f32 = (30.0 as Scalar).to_radians();
 pub const CHARACTER_GRAVITY_SCALE: f32 = 1.5;
