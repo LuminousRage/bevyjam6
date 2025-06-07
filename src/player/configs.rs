50a5b06e7248bed66f8ca48d7ac7032ab7376996
4321louis <45321dumkof@gmail.com>
2025-06-07 17:01:45 +1000

    Dash speed increase

--- src/player/configs.rs
+++ src/player/configs.rs
@@ -2,12 +2,12 @@ use avian2d::math::Scalar;
 use bevy::input::keyboard::KeyCode;
 
 pub const MOVEMENT_SPEED: f32 = 500.0;
-pub const DASH_SPEED_MODIFIER: f32 = 80.;
+pub const DASH_SPEED_MODIFIER: f32 = 130.;
 pub const JUMP_IMPULSE: f32 = 1000.0;
 pub const MOVEMENT_DAMPING: f32 = 6.0;
 pub const MAX_SLOPE_ANGLE: f32 = (30.0 as Scalar).to_radians();
 pub const CHARACTER_GRAVITY_SCALE: f32 = 1.5;
-pub const DASH_DURATION_MILLISECONDS: u64 = 200;
+pub const DASH_DURATION_MILLISECONDS: u64 = 216;
 pub const JUMP_DURATION_MILLISECONDS: u64 = 400;
 pub const DASH_COOLDOWN_DURATION_MILLISECONDS: u64 = 300;
 
