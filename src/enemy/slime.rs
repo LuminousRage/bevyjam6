02f3acf7199c6e6f4aea98ad5ef798509f49a5a2
LuminousRage <56435875+LuminousRage@users.noreply.github.com>
2025-06-09 04:18:55 +1000

    ✨ feat: set slime hibox immunity to 0

--- src/enemy/slime.rs
+++ src/enemy/slime.rs
@@ -84,7 +84,7 @@ pub fn slime(slime_assets: &SlimeAssets, translation: Vec3, is_red: bool) -> imp
             hitbox_prefab(
                 Collider::circle(60.0),
                 enemy_hit_boxes(),
-                0.5,
+                0.0,
                 if is_red { 15.0 } else { 8.0 },
                 Transform::default(),
             )
